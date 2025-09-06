0850f98be8c49cacb5b53ec1676fa840653b4308
Alimjan <alimujiang1414@qq.com>
2025-09-06 16:15:49 +0200

    add log

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ serve({
     fetch(request){
         const url = new URL(request.url);
         if (url.pathname === '/'){
+            console.log("Received request at /");
             return new Response(JSON.stringify({"Status": "OK"}), {
                 headers: {'content-type': 'application/json'}
             });
         } else {
+            console.log(`Received request at ${url.pathname}`);
             return new Response('Not found', {status: 404});
         }
         
