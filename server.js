e6a4985189cf58cbc6c4262a4d6cd74c5388b6c7
Alimjan <alimujiang1414@qq.com>
2025-09-06 16:55:34 +0200

    use hono

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-import {serve} from 'bun'
+import { Hono } from 'hono'
 
-serve({
-    fetch(request){
-        const url = new URL(request.url);
-        if (url.pathname === '/'){
-            console.log("Received request at /");
-            return new Response(JSON.stringify({"Status": "OK-new"}), {
-                headers: {'content-type': 'application/json'}
-            });
-        } else {
-            console.log(`Received request at ${url.pathname}`);
-            return new Response('Not found', {status: 404});
-        }
-        
-    },
-    port: 3000,
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
 })
 
-console.log (`Server is running.`);
+export default app
